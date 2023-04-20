40e852cf2f487f18b508df9b7f93c87a86b9ba7e
Terry Wambui <shikuwambui@gmail.com>
2023-04-20 21:51:34 +0300

    even or odd

--- python-projects/tip-calculator.py
+++ python-projects/tip-calculator.py
@@ -2,7 +2,7 @@ print("Welcome to the tip calculator")
 bill = float(input("what was the total bill? $"))
 tip = int(input("what percentage tip would you like to give? 10, 12, or 15? "))
 people = int(input("how many people will split the bill? "))
-
+30
 bill_with_tip = tip / 100 * bill + bill
 bill_per_person = bill_with_tip / people
 amount = "{:.2f}" .format(bill_per_person)
