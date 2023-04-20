6a41e9eb28716cf0bf8db9b0db4cee5c72219712
Terry Wambui <shikuwambui@gmail.com>
2023-04-20 22:02:28 +0300

    are you tall enough

---
+++ python-projects/rollercoster.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoster!")
+height = int(input("what is your height in cm? "))
+
+if height >= 120:
+    print("yay! you are tall enough to ride!")
+else:
+    print("sorry, you need to get taller")
