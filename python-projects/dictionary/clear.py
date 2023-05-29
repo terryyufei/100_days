4c02e5ed3079de7dd4a66f34db97bdbbf8427d5d
Terry Wambui <shikuwambui@gmail.com>
2023-05-30 00:30:33 +0300

    list in dictionaries

--- python-projects/dictionary/clear.py
+++ python-projects/dictionary/clear.py
@@ -1,11 +1,6 @@
-def clear_string(input_string):
-    """
-    Removes leading and trailing whitespace from a string.
-    
-    Args:
-        input_string (str): The string to be cleared.
-    
-    Returns:
-        str: The cleared string.
-    """
-    return input_string.strip()
+import os
+
+def clear():
+    # Clear the console screen based on the operating system
+    os.system('cls' if os.name == 'nt' else 'clear')
+
--- python-projects/dictionary/silent_auction.py
+++ python-projects/dictionary/silent_auction.py
@@ -1,19 +1,34 @@
-from clear import clear_string
+from clear import clear
 from art import logo
 print(logo)
 
 print("Welcome to the silent auction")
 
 silent_auction = {}
+auction_end = False
 
-name = input("What is your name? ")
-bid = input("What is your bid? $")
+def find_highest_bidder(bid_list):
+    highest_bid = 0
+    winner = ""
 
-silent_auction[name] = bid
-print(silent_auction)
+    for bidder in bid_list:
+        amount = bid_list[bidder]
+        if amount > highest_bid:
+            highest_bid = amount
+            winner = bidder
+    print("The winner is {} with a bid of ${}".format(winner, highest_bid))
 
-others = input("Are there others who would like to bid? Yes or No ").lower()
+while not auction_end:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? $"))
+    silent_auction[name] = bid
+    others = input('Are there others who would like to bid? "yes" or "no".\n')
 
+    if others == "no":
+        auction_end = True
+        find_highest_bidder(silent_auction)
+    elif others == "yes":
+        clear()
 
 
 
