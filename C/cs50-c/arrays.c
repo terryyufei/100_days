dc73dce542fc34767778eadad90717050a89cc6c
Terry Wambui <shikuwambui@gmail.com>
2023-06-21 23:16:14 +0300

    arrays

---
+++ C/cs50-c/arrays.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <cs50.h>
+/*
+int main(void)
+{
+    int scores[3];
+    scores[0] = 72;
+    scores[1] = 73;
+    scores[2] = 33;
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+}*/
+
+/*
+int main(void)
+{
+    int scores[3];
+    int i;
+    
+    for (i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("Scores: ");
+    }
+
+    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / (float) 3);
+}
+*/
+float average( int array[]);
+const int N = 3;
+
+int main(void)
+{
+    int scores[3];
+    int i;
+    
+    for (i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("Scores: ");
+    }
+
+    printf("Average: %f\n", average(scores));
+}
+
+float average(int array[])
+{
+    int sum = 0;
+    int i;
+
+    for (i = 0; i < N; i++)
+    {
+        sum += array[i];
+    }
+    return sum / (float) N;
+}
