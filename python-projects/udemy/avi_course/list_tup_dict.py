955685e29d578d10eeb41194dd29ee886cb6aed4
Terry Wambui <shikuwambui@gmail.com>
2023-07-07 17:05:00 +0300

    another intro

---
+++ python-projects/udemy/avi_course/list_tup_dict.py
@@ -0,0 +1,44 @@
+# Create a list of names and print the second item.
+names = ["Terry", "Sophia", "bella", "gigi"]
+#print(names[1])
+
+# Create a list of sports and then replace the second item with another sport.
+sports = ["swimming", "Running", "Football", "Basketball"]
+sports[1] = "Rugby"
+#print(sports)
+
+# Create a list containing numbers and delete the fifth number from the array.
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+del numbers[4]
+#print(numbers)
+
+# Create two lists of numbers and merge them.
+names = ["Terry", "Sophia", "bella", "gigi"]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+merged_list = names + numbers
+#print(merged_list)
+
+# Create a list of numbers and find the length, minimum, and maximum.
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+#print(len(numbers))
+#print(min(numbers))
+#print(max(numbers))
+
+# Create a dictionary of students and scores and print out a student’s score.
+students = {'bob': 92, 'Mercy': 85, 'Jane': 98, 'james': 37}
+#print(students['Mercy'])
+
+# Create a dictionary of names and ages and then print out all the keys and values
+name = {'bob': 92, 'Mercy': 85, 'Jane': 98, 'james': 37}
+#print(name)
+
+
+# Create a tuple of your favorite movies
+movies = ("power", "red rose", "black mirror", "fargo", "the witcher")
+#print(movies)
+
+# Create a tuple and print all the items from the first to third index.
+movies = ("power", "red rose", "black mirror", "fargo", "the witcher")
+#print(movies[0:3])
+ 
+
---
+++ python-projects/udemy/avi_course/variable.py
@@ -0,0 +1,37 @@
+name, age = "Terry", 20
+#print(name, age)
+
+# Create two strings and then create a third variable combining both of the two original strings.
+first_name = "Terry"
+last_name = "Sophia"
+name = first_name + " " + last_name
+#print((name + "\n") * 10, end='')
+
+# 1. Write a script that adds 15 and 30 and divides the sum by 2. Print out the answer.
+x, y = 15, 30
+#print ((x + y) / 2)
+
+#  Initialize two variables and use arithmetic operators to add, subtract, multiply, divide, and find the remainder.
+x, y = 15, 30
+#print(x + y)
+#print(y - x)
+#print(x * y)
+#print(y % x)
+
+# 3. Create a variable called name and store your name in it as a string.
+name = "Terry"
+
+# Create three variables in one line and assign each one a different food item.
+breakfast, lunch, dinner = "oatmeal", "rice", "tofu"
+
+# Print out "Hello" ten times using arithmetic operators.
+#print(("Hello" + "\n") * 10, end='')
+
+# Create a string and print the fourth letter of the word.
+name = "Terry"
+print(name[4])
+
+# Create a string and print the letters from index 0 to 5.
+name = "Sophia is in her happy girl era"
+print(name[:6])
+
