7b8cc529dc7e4407db23856f8837685e3cf6709c
Terry Wambui <shikuwambui@gmail.com>
2023-07-08 21:46:26 +0300

    filtering

---
+++ python-projects/CS50/etc/filter_names.py
@@ -0,0 +1,23 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherine"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
+"""
+gryffindors = [
+    student["name"] for student in students if student["house"] == "Gryffindor"
+]
+
+for gryffindor in sorted(gryffindors):
+    print(gryffindor)
+"""
+
+def is_gryffindor(s):
+    return s["house"] == "Gryffindor"
+
+gryffindors = filter(is_gryffindor, students)
+
+for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
+    print(gryffindor["name"])
