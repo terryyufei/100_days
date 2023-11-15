13841af86fc00ebc7f6175db24d2e4c772e80b2e
Terry Wambui <shikuwambui@gmail.com>
2023-11-15 22:30:20 +0300

    into to javascript

---
+++ javascript/freecodecamp/js_algorithms_and_ds/main.js
@@ -0,0 +1,24 @@
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+
+a = 5;
+b = 10;
+c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
