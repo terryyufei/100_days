5f04c169fff8d11e8960872fcc03102d2b13c2d6
Terry Wambui <shikuwambui@gmail.com>
2023-11-17 23:41:02 +0300

    life in weeks

--- web_development/javascript/main.js
+++ web_development/javascript/main.js
@@ -6,3 +6,10 @@ function lifeInWeeks (age) {
 
     console.log ("You have " + days + "days " + weeks + "weeks, and " + months + "months left.");
 }
+
+function bmiCalculator(weight, height) {
+    var bmi = weight / (height * height);
+    return Math.round(bmi);
+}
+
+var bmi = bmiCalculator(65, 1.8);
