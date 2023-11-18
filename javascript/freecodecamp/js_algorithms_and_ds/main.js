6559837fe6bad18b3aec80fbe133c05446a59f86
Terry Wambui <shikuwambui@gmail.com>
2023-11-18 22:39:03 +0300

    switch statement

--- javascript/freecodecamp/js_algorithms_and_ds/main.js
+++ javascript/freecodecamp/js_algorithms_and_ds/main.js
@@ -22,3 +22,63 @@ var titleCaseOver;
 studlyCapVar = 10;
 properCamelCase = "A String";
 titleCaseOver = 9000;
+
+
+
+//USING SWITCH
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    answer = "alpha";
+    break;
+    case 2: 
+    answer = "beta";
+    break;
+    case 3:
+    answer = "gamma";
+    break;
+    case 4:
+    answer = "delta";
+    break;
+  }
+
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+
+
+
+// USING SWITCH WITH A DEFAULT VALUE
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "a":
+    answer = "apple";
+    break;
+    case "b":
+    answer = "bird";
+    break;
+    case "c":
+    answer = "cat"
+    break;
+    default:
+    answer = "stuff";
+    break;
+  }
+
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
