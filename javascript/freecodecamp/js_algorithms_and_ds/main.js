cdfb0b43e79e07f7be3edab1fb5b7a7bd260d391
Terry Wambui <shikuwambui@gmail.com>
2023-11-20 21:31:48 +0300

    objects

--- javascript/freecodecamp/js_algorithms_and_ds/main.js
+++ javascript/freecodecamp/js_algorithms_and_ds/main.js
@@ -137,3 +137,132 @@ const testObj = {
 // Only change code below this line
 const hatValue = testObj.hat;      // Change this line
 const shirtValue = testObj.shirt;    // Change this line
+
+// Accessing Object Properties with Variables
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+
+// Add New Properties to a JavaScript Object
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof";
+
+
+// Delete Properties from a JavaScript Object
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
+
+
+
+// Using Objects for Lookups
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  }; 
+  result = lookup[val];
+
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
+
+
+// Testing Objects for Properties
+
+/*Modify the function checkObj to test if the object passed to the function 
+parameter obj contains the specific property passed to the function parameter checkProp. 
+If the property passed to checkProp is found on obj, return that property's value. If not, return Not Found.*/
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)) {
+    // If the property is found, return its value
+    return obj[checkProp];
+  } else {
+    // If the property is not found, return "Not Found"
+    return "Not Found";
+  }
+  // Only change code above this line
+}
+
+// Manipulating Complex Objects
+/*Add a new album to the myMusic array. Add artist and title strings, 
+release_year number, and a formats array of strings.*/
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+, {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }];
+
+
+
+
+// Accessing Nested Objects
+
+/*Access the myStorage object and assign the contents of the glove box property to the gloveBoxContents variable. 
+Use dot notation for all properties where possible, otherwise use bracket notation.*/
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
